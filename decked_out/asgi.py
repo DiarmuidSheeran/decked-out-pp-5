50630bd2f40e8f46cd3f585bee724149039810da
Diarmuid Sheeran <dssheeran@outlook.com>
2024-01-26 22:07:30 +0000

    Initial Commit

---
+++ decked_out/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for decked_out project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'decked_out.settings')
+
+application = get_asgi_application()
