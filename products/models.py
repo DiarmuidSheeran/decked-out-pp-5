aa6e8a3918f2b4cb61214aa9546fb2714f6fc166
Diarmuid Sheeran <dssheeran@outlook.com>
2024-01-27 13:14:38 +0000

    Added product app, models and logic to generate automatic sku to products

---
+++ products/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.sku:
+            last_sku = Product.objects.order_by('-sku').first()
+            if last_sku:
+                last_sku = int(last_sku.sku)
+            else:
+                last_sku = 0
+            self.sku = str(last_sku + 1).zfill(6)  
+        super(Product, self).save(*args, **kwargs)
