07227543beba07c3054485d1a042f5d394341022
Diarmuid Sheeran <dssheeran@outlook.com>
2024-01-27 20:06:09 +0000

    Created checkout css file and stripe elements js file and populated them with stripe elements logic, added stripe card elements to checkout template

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OS5DgEPBBYTB9R1ljMgfHt54Hj8cQkwCkDDhKXws2iMY2OeE0uDI7BB7OUFM1EpCDHfLqOa2JfDW5dTUWA5so4N00Z9twi4oU',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
