d6a5a5f37537dde90291a1f27c7c231abd1bac5f
Diarmuid Sheeran <dssheeran@outlook.com>
2024-01-29 19:26:31 +0000

    Add dj database to site

--- decked_out/settings.py
+++ decked_out/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
@@ -121,12 +122,16 @@ WSGI_APPLICATION = 'decked_out.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+ }
 
 
 # Password validation
