3f2244fddb320eb4205f19baee11dbf115371029
Diarmuid Sheeran <dssheeran@outlook.com>
2024-01-29 19:48:59 +0000

    Edit allowed host, try deployment 2

--- decked_out/settings.py
+++ decked_out/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-diarmuidshe-deckedoutpp-07qedhcrluo.ws-eu107.gitpod.io', 'decked-out-tcg-store-b1147b8f9a0a.herokuapp.com/']
+ALLOWED_HOSTS = ['8000-diarmuidshe-deckedoutpp-07qedhcrluo.ws-eu107.gitpod.io', 'decked-out-tcg-store-b1147b8f9a0a.herokuapp.com']
 
 
 # Application definition
