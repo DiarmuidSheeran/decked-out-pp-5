84209ae5fc7083565495191bcdb95a7d7c237ba4
Diarmuid Sheeran <dssheeran@outlook.com>
2024-01-31 13:34:47 +0000

    Add clearance boolean field to products model, update products template to show clearnce items

--- products/models.py
+++ products/models.py
@@ -24,6 +24,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     promotion_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     is_on_promotion = models.BooleanField(default=False)
+    clearance = models.BooleanField(default=False)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
