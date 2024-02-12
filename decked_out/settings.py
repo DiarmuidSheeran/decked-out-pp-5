9d802b2f3163336d862f1de51569871c657566a3
Diarmuid Sheeran <dssheeran@outlook.com>
2024-02-12 08:55:19 +0000

    Add fix for email sending

--- decked_out/settings.py
+++ decked_out/settings.py
@@ -213,11 +213,12 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'deckedouttcgstore@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
+    EMAIL_USE_TLS = False
+    EMAIL_USE_SSL = True
+    EMAIL_PORT = 465
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 
