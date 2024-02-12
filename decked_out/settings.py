db062f8620dbac42d9bd49b5e5e7317e47778114
Diarmuid Sheeran <dssheeran@outlook.com>
2024-02-12 09:31:15 +0000

    Another email fix

--- decked_out/settings.py
+++ decked_out/settings.py
@@ -215,8 +215,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
