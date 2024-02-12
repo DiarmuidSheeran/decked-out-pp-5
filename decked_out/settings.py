73c7a62a816434d44cb3d5f78ca84f51158e87c9
Diarmuid Sheeran <dssheeran@outlook.com>
2024-02-12 12:26:08 +0000

    Try another fix for sending emails

--- decked_out/settings.py
+++ decked_out/settings.py
@@ -208,7 +208,7 @@ STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
 
-if 'DEVELOPMENT' in os.environ:
+if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'deckedouttcgstore@example.com'
 else:
