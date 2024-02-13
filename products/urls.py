e7a168796716fe13aa6a29e14ab5898ff4b2b0d2
Diarmuid Sheeran <dssheeran@outlook.com>
2024-02-13 09:18:22 +0000

    Add edit product view, url path and template to site

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:product_id>', views.product_detail, name='product_detail'),
     path('products/<str:sku>/like/', views.wishlist, name='wishlist'),
     path('add/', views.add_product, name='add_product'),
+     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
