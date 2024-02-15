0ba99b12509f19eee3fd58b7a3d275776a0b910e
Diarmuid Sheeran <dssheeran@outlook.com>
2024-02-15 16:16:43 +0000

    Edit index template to include casousels dispalying links to products and categorys using slideshow

--- home/views.py
+++ home/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-
+from products.models import Product
 # Create your views here.
 
 def index(request):
+    best_sellers = Product.objects.order_by('-productstatistics__total_sold')[:5]
+    special_offer_products = Product.objects.filter(is_on_promotion=True)
+    clearance_products = Product.objects.filter(clearance=True)
+    new_arrival_products = Product.objects.filter(new_arrival=True)
 
-    return render(request, 'home/index.html')
+    context = {
+        'best_sellers': best_sellers,
+        'special_offer_products': special_offer_products,
+        'clearance_products': clearance_products,
+        'new_arrival_products': new_arrival_products,
+    }
+
+    return render(request, 'home/index.html', context)
 
